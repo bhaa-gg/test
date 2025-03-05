c87306f9dd8b34e22392c58c95b5bc7c21937852
Bahaa Wafy <bhaatiti281@gmail.com>
2025-03-05 03:15:22 +0200

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import React from "react";
 const Home = () => {
   return (
     <div className="">
-      <Tse/>
-      
-    <div className=""></div>
-      <Tse/>
-    <div className=""></div>
+      <Tse />
+
+      <div className=""></div>
+      <Tse />
+      <div className=""></div>
     </div>
   );
 };
