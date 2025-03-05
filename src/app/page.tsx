dde1a58cf847bc59d9df4a95b937e376b58d8193
Bahaa Wafy <bhaatiti281@gmail.com>
2025-03-05 03:06:36 +0200

    dssadssss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,8 @@ import React from "react";
 const Home = () => {
   return (
     <div className="">
-<<<<<<< HEAD
-    <div className="w-full">
         <TodayProperts />
         <ProductTable />
-    </div>
-=======
->>>>>>> cf98e5234d4b5225672f3957d161b9f4314e9557
     </div>
   );
 };
