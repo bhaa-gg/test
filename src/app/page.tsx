86e9614f366344516827121c816f6bf5c98db937
Bahaa_Wafy <114358319+bhaa-gg@users.noreply.github.com>
2025-03-05 02:57:48 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="">
-    <div className="">
+    <div className="w-full">
         <TodayProperts />
         <ProductTable />
         <ProductTable />
